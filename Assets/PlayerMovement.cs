5f4bda67cbb4d764cfe01949ee9a42bbcbc89672
Essi Supponen <essi.supponen@gmail.com>
2022-09-02 21:44:56 +0300

    Only jump after ground

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -5,11 +5,15 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
 	private Rigidbody2D player;
+	private BoxCollider2D ground;
+	
+	[SerializeField] private LayerMask jumpableGround;
 	
     // Start is called before the first frame update
     void Start()
     {
         player = GetComponent<Rigidbody2D>();
+		ground = GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
@@ -18,8 +22,13 @@ public class PlayerMovement : MonoBehaviour
 		float dirX = Input.GetAxis("Horizontal");
 		player.velocity = new Vector2(5f * dirX, player.velocity.y);
 		
-        if (Input.GetButtonDown("Jump")) {
+        if (Input.GetButtonDown("Jump") && IsGrounded()) {
 			player.velocity = new Vector2(player.velocity.x, 10f);
 		}
     }
+	
+	private bool IsGrounded()
+	{
+		return Physics2D.BoxCast(ground.bounds.center, ground.bounds.size, 0f, Vector2.down, .1f, jumpableGround);
+	}
 }
