7d889c0639a10b8bdfa393225f3bd51e64ff4c8f
Essi Supponen <essi.supponen@gmail.com>
2022-09-04 11:14:26 +0300

    Auto stash before merge of "main" and "origin/main"

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,7 +5,13 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
 	private Rigidbody2D player;
+	private SpriteRenderer sprite;
 	private BoxCollider2D ground;
+	private Animator animator;
+
+	private float dirX = 0;
+	[SerializeField] private float walkSpeed = 5f;
+	[SerializeField] private float jumpForce = 12f;
 	
 	[SerializeField] private LayerMask jumpableGround;
 	
@@ -14,18 +20,40 @@ public class PlayerMovement : MonoBehaviour
     {
         player = GetComponent<Rigidbody2D>();
 		ground = GetComponent<BoxCollider2D>();
+		animator = GetComponent<Animator>();
+		sprite = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-		float dirX = Input.GetAxis("Horizontal");
-		player.velocity = new Vector2(5f * dirX, player.velocity.y);
+		dirX = Input.GetAxis("Horizontal");
+		player.velocity = new Vector2(walkSpeed * dirX, player.velocity.y);
 		
         if (Input.GetButtonDown("Jump") && IsGrounded()) {
-			player.velocity = new Vector2(player.velocity.x, 12f);
+			player.velocity = new Vector2(player.velocity.x, jumpForce);
 		}
+
+		UpdateAnimation();
     }
+
+	private void UpdateAnimation()
+	{
+		if (dirX > 0f)
+		{
+			animator.SetBool("running", true);
+			sprite.flipX = false;
+		}
+		else if (dirX < 0f)
+		{
+			animator.SetBool("running", true);
+			sprite.flipX = true;
+		}
+		else
+		{
+			animator.SetBool("running", false);
+		}
+	}
 	
 	private bool IsGrounded()
 	{
